370ad6e40f3a35909c87c450ae1273b15a927068
Jimmy <39@.com>
2022-12-08 20:10:44 -0500

    Add timestep denoising

--- paella.py
+++ paella.py
@@ -131,30 +131,10 @@ def train(args):
         unwrapped_model = accelerator.unwrap_model(model)
         loaded = torch.load(f"models/{args.run_name}/pytorch_model.bin", map_location='cpu')
         unwrapped_model.load_state_dict(loaded)
-
-        # accelerator.save_state(f"models/{args.run_name}/")
-        # import sys
-        # sys.exit()
     else:
         losses = []
         accuracies = []
         start_step, total_loss, total_acc = 0, 0, 0
-
-    '''
-    for scheduler in accelerator._schedulers:
-        # scheduler.scheduler.state_dict().to('cpu')
-        for param in scheduler.scheduler.__dict__.values():
-            if isinstance(param, torch.Tensor):
-                param.data = param.data.to('cpu')
-                if param._grad is not None:
-                    param._grad.data = param._grad.data.to('cpu')
-    for optimizer in accelerator._optimizers:
-        # optimizer.optimizer.state_dict().to('cpu')
-        for state in optimizer.optimizer.state.values():
-            for k, v in state.items():
-                if isinstance(v, torch.Tensor):
-                    state[k] = v.to('cpu')
-    '''
 
     model_ema = None
     if args.ema:
@@ -218,45 +198,49 @@ def train(args):
         # print('TEXT EMBEDS', text_embeddings_uncond.size(), text_embeddings_full_uncond.size())
         # import sys
         # sys.exit()
+        # Iterate backwards over the image, with more noise each time.
+        for timestep_r in torch.linspace(0.9, 0., args.timesteps):
+            image_indices = encode(vqmodel, images)
 
-        image_indices = encode(vqmodel, images)
+            # r = torch.rand(images.size(0), device=device)
+            r = timestep_r.repeat(images.size(0)).to(device)
+            noised_indices, mask = model.module.add_noise(image_indices, r)
 
-        r = torch.rand(images.size(0), device=device)
-        noised_indices, mask = model.module.add_noise(image_indices, r)
+            if (
+                np.random.rand() < 0.1
+            ):  # 10% of the times -> unconditional training for classifier-free-guidance
+                # Old method:
+                # text_embeddings = images.new_zeros(images.size(0), 2048)
+                # text_embeddings_full = images.new_zeros(images.size(0), 77, 2048)
+                # New method:
+                text_embeddings = text_embeddings_uncond
+                text_embeddings_full = text_embeddings_full_uncond
 
-        if (
-            np.random.rand() < 0.1
-        ):  # 10% of the times -> unconditional training for classifier-free-guidance
-            # Old method:
-            # text_embeddings = images.new_zeros(images.size(0), 2048)
-            # text_embeddings_full = images.new_zeros(images.size(0), 77, 2048)
-            # New method:
-            text_embeddings = text_embeddings_uncond
-            text_embeddings_full = text_embeddings_full_uncond
+            pred = model(noised_indices, text_embeddings, r, text_embeddings_full)
+            image_indices = image_indices.to(device)
+            image_indices_decoded = decode(vqmodel, image_indices)
+            out_flat = pred.permute(0, 2, 3, 1).reshape(-1, pred.size(1))
+            out_flat = gumbel_sample(out_flat, temperature=1.0)
+            out_flat = out_flat.view(pred.size(0), *pred.shape[2:])
+            pred_decoded = decode(vqmodel, out_flat)
+            loss = criterion(pred_decoded, image_indices_decoded)
+            loss_adjusted = loss * args.accum_grad
+            loss_adjusted = Variable(loss_adjusted, requires_grad=True)
 
-        pred = model(noised_indices, text_embeddings, r, text_embeddings_full)
-        image_indices = image_indices.to(device)
-        image_indices_decoded = decode(vqmodel, image_indices)
-        out_flat = pred.permute(0, 2, 3, 1).reshape(-1, pred.size(1))
-        out_flat = gumbel_sample(out_flat, temperature=1.0)
-        out_flat = out_flat.view(pred.size(0), *pred.shape[2:])
-        pred_decoded = decode(vqmodel, out_flat)
-        loss = criterion(pred_decoded, image_indices_decoded)
-        loss_adjusted = loss * args.accum_grad
-        loss_adjusted = Variable(loss_adjusted, requires_grad=True)
+            accelerator.backward(loss_adjusted)
+            grad_norm = torch.nn.utils.clip_grad_norm_(model.parameters(), 5).item()
 
-        accelerator.backward(loss_adjusted)
-        grad_norm = torch.nn.utils.clip_grad_norm_(model.parameters(), 5).item()
+            optimizer.step()
+            scheduler.step()
+            optimizer.zero_grad()
 
-        optimizer.step()
-        scheduler.step()
-        optimizer.zero_grad()
+            if timestep_r == 0.1:
+                acc = (pred.argmax(1) == image_indices).float()
+                acc = acc.mean()
 
-        acc = (pred.argmax(1) == image_indices).float()
-        acc = acc.mean()
-
-        total_loss += loss_adjusted.item()
-        total_acc += acc.item()
+                total_loss += loss_adjusted.item()
+                total_acc += acc.item()
+        
         if accelerator.is_main_process:
             log = {
                 "loss": total_loss / (step + 1),
@@ -485,6 +469,7 @@ if __name__ == "__main__":
     args.offload = False
     args.n_nodes = 1
     args.devices = [0,1,2,3,4,5,6]
+    args.timesteps = 10
 
     # Testing:
     # args.dataset_path = '/home/user/Programs/Paella/models/6.tar'
